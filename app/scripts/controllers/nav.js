a4d3ad0933989f5d1720d2437634e407feda1e53
Prajwal Nayak <theprajwalnayak@gmail.com>
2018-10-07 11:48:46 +0530

    Adding dist

--- app/scripts/controllers/nav.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-
-angular.module('TheNayakNews').controller('navController', ['$scope', '$location', function ($scope, $location) {
-  $scope.isCurrentPath = function (path) {
-    return $location.path() == path;
-  };
-
-}]);
